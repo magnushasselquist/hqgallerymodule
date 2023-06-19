323cc6835f4818eff643ce79f15932ca1fdfbb9d
Magnus Hasselquist <magnus.hasselquist@gmail.com>
2023-06-19 18:41:21 +0200

    try to use content-prepare..

--- tmpl/default.php
+++ tmpl/default.php
@@ -2,9 +2,15 @@
 
 defined('_JEXEC') or die;
 
+// Prepare the output
+$output = "";
+
+// Retrieve the value of the "prepare_content" parameter
+$prepareContent = $params->get('prepare_content', 0);
+
 // Access to module parameters
 $folder = $params->get('folder', '');
-echo "Folder: " .$folder;
+// $output .= "Folder: " .$folder;
 
 $scan = scandir('images/'.$folder);
 $numberofFiles = 0;
@@ -12,7 +18,7 @@ $numberofFiles = 0;
 // PRINT FOLDERS
 foreach($scan as $file) {
    if (is_dir("images/$folder/$file")) {
-      echo $file.', ';
+    $output .= $file.', ';
    } else {
     $numberofFiles = $numberofFiles +1;
    }
@@ -20,8 +26,21 @@ foreach($scan as $file) {
 
 if ($numberofFiles > 0) {
     // echo "Det finns också: ". $numberofFiles. " filer.";
-    echo "{gallery}".$folder."{/gallery}";
+    $output .= "{gallery}".$folder."{/gallery}";
 } else {
-    echo "Det finns inga filer i mappen.";
+    $output .= echo "Det finns inga filer i mappen.";
 }
+
+// Conditionally prepare the content if the switch is enabled
+if ($prepareContent == 1) {
+  // Get the application instance
+  $app = Joomla\CMS\Factory::getApplication();
+
+  // Prepare the content using Joomla's content preparation methods
+  use Joomla\CMS\HTML\HTMLHelper;
+  $output = HTMLHelper::_('content.prepare', $output, '', 'mod_hqgallerymodule');
+}
+
+// Finally, print the result
+echo $output;
 ?>
