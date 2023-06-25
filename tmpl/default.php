d85cb77f68a13de014061bf2d0ebf896d0fc0a2f
Magnus Hasselquist <magnus.hasselquist@gmail.com>
2023-06-25 18:05:14 +0200

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -8,11 +8,81 @@ use Joomla\CMS\HTML\HTMLHelper;
 // ModuleHelper behövs för att läsa ut modul-id-t
 use Joomla\CMS\Helper\ModuleHelper;
 $moduleId = $module->id;
-echo $moduleId; // DEBUG
+// echo $moduleId; // DEBUG
 
-// See if this page was initiated by someone requesting a certain folder
-if (isset($_GET["m"]) && $moduleId == $_GET["m"]) {
+// Depending on POST or GET or no request:
+if (isset($_GET["m"]) && $moduleId == $_GET["m"]) { 
+    // someone requesting to VIEW a certain folder
     $target=$_GET["g"];
+} else if (isset($_POST["m"]) && $moduleId == $_POST["m"]) { 
+    // USER wants to UPLOAD pictures to this module and folder
+    $target=$_POST["g"];
+    
+    // Configure upload directory and allowed file types
+    $upload_dir = $target;
+    $allowed_types = array('jpg', 'png', 'jpeg', 'gif'); // TODO: INTEGRATE WITH JOOMLA?!
+    
+    // Define maxsize for files i.e 2MB
+    $maxsize = 200 * 1024 * 1024; // TODO: GET THIS FROM PHP OR JOOMLA
+
+    // Checks if user sent an empty form
+    if(!empty(array_filter($_FILES['files']['name']))) {
+
+        // Loop through each file in files[] array
+        foreach ($_FILES['files']['tmp_name'] as $key => $value) {
+            
+            $file_tmpname = $_FILES['files']['tmp_name'][$key];
+            $file_name = $_FILES['files']['name'][$key];
+            $file_size = $_FILES['files']['size'][$key];
+            $file_ext = pathinfo($file_name, PATHINFO_EXTENSION);
+
+            // Set upload file path
+            $filepath = $upload_dir.$file_name; // TODO: ADD DATE/TIME/RND something to make more unique?
+
+            // Check file type is allowed or not
+            if(in_array(strtolower($file_ext), $allowed_types)) {
+
+                // Verify file size - 2MB max
+                if ($file_size > $maxsize)        
+                    echo "Error: File size is larger than the allowed limit.";
+
+                // If file with name already exist then append time in
+                // front of name of the file to avoid overwriting of file
+                if(file_exists($filepath)) {
+                    $filepath = $upload_dir.time().$file_name;
+                    
+                    if( move_uploaded_file($file_tmpname, $filepath)) {
+                        echo "{$file_name} successfully uploaded <br />";
+                    }
+                    else {                    
+                        echo "Error uploading {$file_name} <br />";
+                    }
+                }
+                else {
+                
+                    if( move_uploaded_file($file_tmpname, $filepath)) {
+                        echo "{$file_name} successfully uploaded <br />";
+                    }
+                    else {                    
+                        echo "Error uploading {$file_name} <br />";
+                    }
+                }
+            }
+            else {
+                
+                // If file extension not valid
+                echo "Error uploading {$file_name} ";
+                echo "({$file_ext} file type is not allowed)<br / >";
+            }
+        }
+    }
+    else {
+        
+        // If no files selected
+        echo "No files selected.";
+    }
+
+
 } else {
     $target = '';
 }
@@ -74,10 +144,11 @@ if ($numberofFiles > 0) {
     // $output .= "Det finns inga filer i mappen.";
 }
 
-$output .= '<form action="" method="POST" enctype="multipart/form-data">
+$output .= '<h3>Ladda upp bilder</h3><form action="" method="POST" enctype="multipart/form-data">
 <input type="file" name="files[]" multiple>
 <input type="submit" name="Upload" value="Upload" >
-<input type="hidden" name="t" value="'.$folder.'"
+<input type="hidden" name="g" value="'.$folder.'">
+<input type="hidden" name="m" value="'.$moduleId.'">
 </form>';
 
 // Conditionally prepare the content if the switch is enabled
